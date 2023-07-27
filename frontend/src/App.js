14def9f299c19bfce995775cbceb00ea3a4113fc
Chun Yu "Eric" Wu <ericchunyuwu@gmail.com>
2023-07-27 14:41:03 +0800

    modify App.js sidebar route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import Navbar from "./components/Navbar";
 import Footer from './components/Footer';
 import Sidebar from "./components/Sidebar";
 
-import React, { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Album from './pages/Album';

167feaae417b12f0d1035d73ba477a1d4e438a11
EricWu <ericchunyuwu@gmail.com>
2023-07-31 15:37:59 +0800

    journey Modal in progress

--- frontend/src/components/JourneyThumbnail.js
+++ frontend/src/components/JourneyThumbnail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../css/scheduleStyles.css'
+import '../css/journeyThumbnail.css'
 
 const thumbNailStyle = {
     backgroundImage: 'url(../images/street.jpg)',
@@ -8,16 +8,24 @@ const thumbNailStyle = {
     width: '100%',
 }
 
+const moreDetailStyle = {
+    height: '2rem',
+    width: '2rem',
+}
 
 
-
-function JourneyThumbnail() {
+function JourneyThumbnail({onClick}) {
     return (
         <div
-            className="col-md-6 col-lg-4 col-xl-3 p-2 d-flex align-items-center justify-content-center">
+            className="col-md-6 col-lg-4 col-xl-3 p-2 d-flex align-items-center justify-content-center" onClick={onClick}>
             <div className="rounded shadow"
                 style={thumbNailStyle}>
-                <div className="overlay rounded d-flex justify-content-center align-items-center">
+                <div className="overlay rounded d-flex justify-content-center align-items-center position-relative">
+                    <div className="position-absolute top-0 end-0 p-1 m-3 rounded more-detail"
+                        style={moreDetailStyle}>
+                        <i
+                            className="bi bi-three-dots text-secondary fs-5 position-absolute top-50 start-50 translate-middle"></i>
+                    </div>
                     <div>金瓜石之旅</div>
                 </div>
             </div>
--- frontend/src/pages/Journey.js
+++ frontend/src/pages/Journey.js
@@ -1,6 +1,142 @@
-import React from 'react';
-import '../css/scheduleStyles.css';
+import React, { useState, useRef, useEffect } from 'react';
+import { Modal } from 'bootstrap';
+
+import '../css/journey.css';
 import JourneyThumbnail from '../components/JourneyThumbnail';
+
+
+
+const AddNewJourney = () => {
+
+    const plusIconStyle = {
+        fontSize: '2rem',
+        color: '#2F4E63',
+    }
+
+    const addNewThumbnailStyle = {
+        width: '100%',
+        height: '200px',
+        backgroundColor: 'gray',
+    }
+
+    return (
+        <div
+            className="col-md-6 col-lg-4 col-xl-3 p-2 d-flex align-items-center justify-content-center">
+            <div style={addNewThumbnailStyle}
+                className="d-flex align-items-center justify-content-center rounded shadow add-new-journey"
+                id="addNewBox">
+                <i className="bi bi-plus-lg" style={plusIconStyle}></i>
+            </div>
+        </div>
+    )
+}
+
+const MyJourneyModal = ({showModal, journeyModalRef, setShowModal}) => {
+
+    const [currentJourney, setCurrentJourney] = useState(
+        {
+            journeyTitle: '台北之旅',
+            textContent: '台北好好玩',
+            images: {
+                1: '../images/street.jpg',
+                2: '../images/street.jpg',
+                3: '../images/street.jpg',
+            },
+        }
+    );
+
+    useEffect(() => {
+        if (showModal) {
+            if (journeyModalRef.current) {
+                if (journeyModalRef) {
+                    const journeyModel = new Modal(journeyModalRef.current);
+                    journeyModel.show();
+                }
+            }
+        }
+        else {
+            if (journeyModalRef.current) {
+                const journeyModel = new Modal(journeyModalRef.current);
+                journeyModel.hide();
+                return (() => setShowModal(false))
+            }
+        }
+    }, [showModal, journeyModalRef]);
+
+    const modelHeaderStyle = {
+        height: '200px',
+        backgroundImage: 'url(../images/street.jpg)',
+        backgroundSize: 'cover',
+    }
+
+    const journeyContainerStyle = {
+        width: '100%',
+        minHeight: '500px',
+        background: 'linear-gradient(135deg, rgba(235,244,245,0.5) 57%, rgba(181,198,224,0.5) 100%)',
+    }
+
+    const mapContainerStyle = {
+        width: '100%',
+        height: '500px',
+        border: 'lightblue solid',
+    }
+
+    const imageContainerStyle = {
+        width: '100%',
+        height: '200px',
+    }
+
+    return (
+        <div class="modal fade" id="journeyModal" tabIndex="-1" aria-labelledby="journeyModalLabel" aria-hidden="true" ref={journeyModalRef}>
+            <div class="modal-dialog modal-dialog-scrollable modal-xl modal-fullscreen-lg-down">
+                <div class="modal-content">
+                    <div class="modal-header"
+                        style={modelHeaderStyle}>
+                        {/* <h1 class="modal-title fs-5 align-self-end text-white" id="journeyModalLabel">Modal title</h1> */}
+                        <button type="button" class="btn-close btn-close-white align-self-start" data-bs-dismiss="modal"
+                            aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div id="journeyRegView" class="">
+                            <div id="contentHeader" class="d-flex align-items-center">
+                                <h1>Model</h1>
+                                <div class="h5 ms-auto active-text"><i class="bi bi-pencil-square"></i><span
+                                    class="ms-2">編輯</span></div>
+                            </div>
+                            <div id="journeyContainer" class="p-3 rounded shadow" style={journeyContainerStyle}>
+
+                            </div>
+                            <div id="mapContainer" class="mt-3" style={mapContainerStyle} >
+                            </div>
+
+                            <div class="h2 mt-3">備註</div>
+                            <div id="textContent" class="mt-3">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, recusandae? Veniam, error
+                                iure et voluptas quae enim! Amet consequuntur dolorum odit, vero nulla mollitia suscipit libero,
+                                iste ullam, inventore quisquam?
+                                Saepe doloremque labore rem cum reprehenderit totam, maxime eaque. Repudiandae maiores eligendi
+                            </div>
+                            <div class="h2 mt-3">相片</div>
+                            <div id="imageContainer" class="mt-3 image-overflow rounded shadow"
+                                style={imageContainerStyle}>
+                                <img src="./src/images/street.jpg" alt="" class="h-100" />
+                                <img src="./src/images/street.jpg" alt="" class="h-100" />
+                                <img src="./src/images/street.jpg" alt="" class="h-100" />
+                                <img src="./src/images/street.jpg" alt="" class="h-100" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
 
 function Journey() {
 
@@ -13,8 +149,23 @@ function Journey() {
         listStyleType: 'none',
     }
 
+
+    const journeyModalRef = useRef(null);
+    const [showModal, setShowModal] = useState(false);
+
+
+    const handleShowModal = () => {
+        setShowModal(true);
+    }
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    }
+
+
+
     return (
-        <div className="container-fluid shadow p-0  mx-0 mb-3 bg-white" id="bodyContainer">
+        <div className="container-lg shadow p-0 mb-3 bg-white" id="bodyContainer">
             <div className="d-flex flex-nowrap row container-fluid m-0 p-0 bg-white" id="contentContainer">
 
                 {/* <!-- side bar --> */}
@@ -103,63 +254,32 @@ function Journey() {
                 <div className="flex-fill px-0 justify-content-center" id="content">
                     <div className="m-5">
                         <div className="d-flex align-items-end">
-                            <a href="" className="rm-link-style">
+                            <a href="#" className="rm-link-style">
                                 <div className="h1">我的行程</div>
                             </a>
 
                             <div className="h5 ms-auto active-text add-new-journey"><i className="bi bi-plus-lg"></i><span className="ms-1">建立行程</span>
                             </div>
                         </div>
-                        <hr/>
-                            <div className="container-fluid">
-                                <div className="row">
-                                    <JourneyThumbnail />
-                                    <JourneyThumbnail />
-                                    <JourneyThumbnail />
-                                    <JourneyThumbnail />
-                                    <JourneyThumbnail />
-                                    <JourneyThumbnail />
-                                    <JourneyThumbnail />
-                                    <JourneyThumbnail />
-                                    <JourneyThumbnail />
-                                    {/* <div className="col-md-6 col-lg-4 col-xl-3 p-2 d-flex align-items-center justify-content-center dropdown"
-                                        type="button" id="journey1">
-
-                                        <div className="rounded shadow"
-                                            style="background-image: url(./src/images/street.jpg); background-size: cover; height: 200px; width: 100%;">
-                                            <div className="overlay rounded d-flex justify-content-center align-items-center position-relative"
-                                                id="overlay">
-                                                <div className="position-absolute top-0 end-0 p-1 m-3 rounded" id="moreDetail"
-                                                    style="height: 2rem; width: 2rem;">
-                                                    <i
-                                                        className="bi bi-three-dots text-secondary fs-5 position-absolute top-50 start-50 translate-middle"></i>
-                                                </div>
-                                                <div>金瓜石之旅</div>
-                                            </div>
-                                        </div>
-                                    </div> */}
-                                    <div
-                                        className="col-md-6 col-lg-4 col-xl-3 p-2 d-flex align-items-center justify-content-center">
-                                        <div style={{
-                                            width: '100%',
-                                            height:'200px',
-                                            backgroundColor:'gray',
-                                        }}
-                                            className="d-flex align-items-center justify-content-center rounded shadow add-new-journey"
-                                            id="addNewBox">
-                                            <i className="bi bi-plus-lg" style={{
-                                                fontSize: '2rem', 
-                                                color: '#2F4E63',
-                                            }}></i>
-                                        </div>
-                                    </div>
-                                    
-                                </div>
+                        <hr />
+                        <div className="container-fluid">
+                            <div className="row">
+                                <JourneyThumbnail key={1} onClick={handleShowModal} />
+                                <JourneyThumbnail key={2} />
+                                <JourneyThumbnail key={3} />
+                                <JourneyThumbnail key={4} />
+                                <JourneyThumbnail key={5} />
+                                <JourneyThumbnail key={6} />
+                                <JourneyThumbnail key={7} />
+                                <JourneyThumbnail key={8} />
+                                <JourneyThumbnail key={9} />
+                                <AddNewJourney />
                             </div>
-
+                        </div>
                     </div>
                 </div>
             </div>
+            <MyJourneyModal journeyModalRef={journeyModalRef} showModal={showModal} setShowModal={setShowModal}/>
         </div>
     );
 }

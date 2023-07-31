99ea8f177307320593b136f5c8ba877b7c32b534
Chun Yu Eric Wu <ericchunyuwu@gmail.com>
2023-08-01 00:01:06 +0800

    fix journeyModal

--- frontend/src/pages/Journey.js
+++ frontend/src/pages/Journey.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-// import { Modal } from 'bootstrap';
+import { Modal } from 'bootstrap';
 
 import '../css/journey.css';
 import JourneyThumbnail from '../components/JourneyThumbnail';
@@ -33,6 +33,87 @@ const AddNewJourney = () => {
 
 
 
+const JourneyModel = ({ handleCloseModal }) => {
+
+    const modalHeaderStyle = {
+        height: '200px',
+        backgroundImage: 'url(../images/street.jpg)',
+        backgroundSize: 'cover',
+    }
+
+    const mapContainerStyle = {
+        width: '100%',
+        height: '500px',
+        border: 'lightblue solid',
+    }
+
+    const imageContainerStyle = {
+        width: '100%',
+        height: '200px',
+    }
+
+    const journeyContainerStyle = {
+        width: '100%',
+        minHeight: '500px',
+        background: 'linear-gradient(135deg, rgba(235,244,245,0.5) 57%, rgba(181,198,224,0.5) 100%)',
+    }
+
+    return (
+        <div class="modal fade" id="journeyModal" tabindex="-1" aria-labelledby="journeyModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-scrollable modal-xl modal-fullscreen-lg-down">
+                <div class="modal-content">
+                    <div class="modal-header"
+                        style={modalHeaderStyle}>
+                        <h1 class="modal-title fs-5 align-self-end text-white" id="journeyModalLabel">Modal title</h1>
+                        <button type="button" class="btn-close btn-close-white align-self-start" onClick={handleCloseModal}
+                            aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div id="journeyRegView" class="">
+                            <div id="contentHeader" class="d-flex align-items-center">
+                                <h1>Model</h1>
+                                <div class="h5 ms-auto active-text"><i class="bi bi-pencil-square"></i><span
+                                    class="ms-2">編輯</span></div>
+                            </div>
+                            <div id="journeyContainer" class="p-3 rounded shadow" style={journeyContainerStyle}>
+
+                            </div>
+                            <div id="mapContainer" style={mapContainerStyle} class="mt-3">
+                            </div>
+
+                            <div class="h2 mt-3">備註</div>
+                            <div id="text-content" class="mt-3">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, recusandae? Veniam, error
+                                iure et voluptas quae enim! Amet consequuntur dolorum odit, vero nulla mollitia suscipit libero,
+                                iste ullam, inventore quisquam?
+                                Saepe doloremque labore rem cum reprehenderit totam, maxime eaque. Repudiandae maiores eligendi
+                            </div>
+                            <div class="h2 mt-3">相片</div>
+                            <div id="imageContainer" class="mt-3 image-overflow rounded shadow"
+                                style={imageContainerStyle}>
+                                <img src="../images/street.jpg" alt="" class="h-100" />
+                                <img src="../images/street.jpg" alt="" class="h-100" />
+                                <img src="../images/street.jpg" alt="" class="h-100" />
+                                <img src="../images/street.jpg" alt="" class="h-100" />
+                                <img src="../images/street.jpg" alt="" class="h-100" />
+                                <img src="../images/street.jpg" alt="" class="h-100" />
+                                <img src="../images/street.jpg" alt="" class="h-100" />
+                                <img src="../images/street.jpg" alt="" class="h-100" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" onClick={handleCloseModal}>Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
 function Journey() {
 
     const sideBarStyle = {
@@ -43,6 +124,22 @@ function Journey() {
     const removeListDefault = {
         listStyleType: 'none',
     }
+
+    const journeyModal = useRef(null);
+
+    useEffect(() => {
+        journeyModal.current = new Modal('#journeyModal');
+    }, [])
+
+    const handleOpenModal = () => {
+        journeyModal.current.show();
+    }
+
+    const handleCloseModal = () => {
+        journeyModal.current.hide();
+    }
+
+
 
     return (
         <div className="container-lg shadow p-0 mb-3 bg-white" id="bodyContainer">
@@ -144,7 +241,7 @@ function Journey() {
                         <hr />
                         <div className="container-fluid">
                             <div className="row">
-                                <JourneyThumbnail key={1} />
+                                <JourneyThumbnail key={1} handleOpenModal={handleOpenModal} />
                                 <JourneyThumbnail key={2} />
                                 <JourneyThumbnail key={3} />
                                 <JourneyThumbnail key={4} />
@@ -159,7 +256,7 @@ function Journey() {
                     </div>
                 </div>
             </div>
-
+            <JourneyModel handleCloseModal={handleCloseModal} />
         </div>
     );
 }

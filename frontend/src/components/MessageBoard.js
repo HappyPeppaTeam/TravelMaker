d25f3766ce760588a85177b38727a4a9a1a0e819
asd0624a <zxc0624a@gmail.com>
2023-08-03 09:15:11 +0800

    fix  MessageBoard.js submit  add new message

--- frontend/src/components/MessageBoard.js
+++ frontend/src/components/MessageBoard.js
@@ -4,15 +4,24 @@ import '../css/MessageBoard.css';
 const MessageBoardInputComponent = () => {
     const [inputValue, setInputValue] = useState({
         message: "",
+        username: "John Doe",
+        photoPath: "../images/default-photo.jpg",
+        userId: 1, // 預設使用者ID，根據實際需求更改
     });
 
+    const [messages, setMessages] = useState([]);
+
     const handleChange = (e) => {
-        setInputValue(
-            {message: e.target.value}
-        );
+        setInputValue({
+            ...inputValue,
+            message: e.target.value,
+        });
     };
+
     const handleSubmit = () => {
-        fetch(`http://localhost/TravelMaker/Backend/routes/script.php?inputValue=${inputValue}`)
+        setMessages([...messages, inputValue]);
+
+        fetch(`http://localhost/TravelMaker/Backend/routes/script.php?inputValue=${inputValue.message}`)
             .then((response) => response.text())
             .then((data) => {
                 console.log(data);
@@ -20,8 +29,12 @@ const MessageBoardInputComponent = () => {
             .catch((error) => {
                 console.error("Error:", error);
             });
-    };
 
+        setInputValue({
+            ...inputValue,
+            message: "",
+        });
+    };
 
     return (
         <div className="MessageBoardInputComponent">
@@ -43,8 +56,21 @@ const MessageBoardInputComponent = () => {
                     />
                 </form>
             </div>
-            <div id="mess">{inputValue.message}</div>
+            <div>
+                {messages.map((messageData, index) => (
+                    <div key={index} className="Message">
+                        <div className="UserImage">
+                            <img src={messageData.photoPath} alt="" />
+                        </div>
+                        <div className="MessageContent">
+                            <div className="Username">{`User ID: ${messageData.userId}`}</div>
+                            <div className="MessageText">{messageData.message}</div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
+
 export default MessageBoardInputComponent;

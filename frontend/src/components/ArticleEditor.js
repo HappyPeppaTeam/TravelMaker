d986b9d2088e103e15c5ed2ab8a533edb3ada6ff
asd0624a <zxc0624a@gmail.com>
2023-08-09 15:20:26 +0800

    create AddArticl.js & ArticleEditor components

--- frontend/src/components/ArticleEditor.js
+++ frontend/src/components/ArticleEditor.js
@@ -1,73 +1,234 @@
-// import React, { useState } from 'react';
-// import Editor from 'ckeditor5-custom-build/build/ckeditor';
-// import { CKEditor } from '@ckeditor/ckeditor5-react';
-// import axios from 'axios'; // 导入Axios
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-// const ArticleEditor = () => {
-//     const [editorData, setEditorData] = useState('<p>Hello from CKEditor 5!</p>');
+function useImage() {
+    const inputRef = useRef(null);
+    const [images, setImages] = useState([]);
+    const [imagesData, setImagesData] = useState([]);
 
-//     const handleEditorReady = (editor) => {
-//         console.log('Editor is ready to use!', editor);
-//     };
+    console.log(imagesData);
+    // 上傳圖片
+    const handleUpload = (e) => {
+        const images = [...e.target.files].map((file) => {
+            return {
+                name: file.name,
+                url: URL.createObjectURL(file),
+            }
+        });
 
-//     const handleEditorChange = (event, editor) => {
-//         const data = editor.getData();
-//         console.log({ event, editor, data });
-//         setEditorData(data);
-//     };
+        const imagesData = e.target.files;
+        setImagesData([...imagesData]);
+        setImages(images);
+    }
 
-//     const handleEditorBlur = (event, editor) => {
-//         console.log('Blur.', editor);
-//     };
+    // 移除單一圖片
+    const handleRemove = (e, imgIndex) => {
+        e.preventDefault();
+        setImages(images.filter((img, index) => {
+            if (index === imgIndex) {
+                URL.revokeObjectURL(img.url);
+            }
+            return index !== imgIndex;
+        }))
+        setImagesData(imagesData.filter((item, index) => {
+            return index !== imgIndex;
+        }))
+    }
 
-//     const handleEditorFocus = (event, editor) => {
-//         console.log('Focus.', editor);
-//     };
+    // 移除全部圖片
+    const handleRemoveAll = (e) => {
+        e.preventDefault();
+        images.forEach((img) => {
+            URL.revokeObjectURL(img.url);
+        });
+        setImages([]);
+        setImagesData([]);
+    }
 
-//     const handleImageUpload = async (editor) => {
-//         const input = document.createElement('input');
-//         input.type = 'file';
-//         input.accept = 'image/*';
-//         input.click();
+    useEffect(() => {
+        if (images.length === 0 && inputRef.current) {
+            inputRef.current.value = "";
+        }
+    }, [images])
 
-//         input.addEventListener('change', async () => {
-//             const file = input.files[0];
-            
-//             if (file) {
-//                 const formData = new FormData();
-//                 formData.append('image', file);
+    return {
+        images,
+        imagesData,
+        handleUpload,
+        handleRemove,
+        handleRemoveAll,
+        inputRef,
+    }
+}
 
-//                 try {
-//                     const response = await axios.post('/http://localhost/TravelMaker/Backend/public/api/postimgurl', formData); // 替换为你的图片上传URL
-//                     const imageUrl = response.data.imageUrl; // 根据你的服务器返回结构获取图片URL
-                    
-//                     editor.model.change(writer => {
-//                         const imageElement = writer.createElement('image', {
-//                             src: imageUrl
-//                         });
+function ArticleEditor() {
+    const [Board, setBoard] = useState('請選擇');
+    const [Location, setLocation] = useState('請選擇');
+    const [Topic, setTopic] = useState('請選擇');
+    const [Title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const { images, imagesData, handleUpload, handleRemove, handleRemoveAll, inputRef } = useImage();
+    const [exceedLimit, setExceedLimit] = useState(false);
 
-//                         editor.model.insertContent(imageElement, editor.model.document.selection);
-//                     });
-//                 } catch (error) {
-//                     console.error('Image upload error:', error);
-//                 }
-//             }
-//         });
-//     };
 
-//     return (
-//         <div className="ArticleEditor">
-//             <h2>Using CKEditor 5 from online builder in React</h2>
-//             <CKEditor
-//                 editor={Editor}
-//                 data={editorData}
-//                 onReady={handleEditorReady}
-//                 onChange={handleEditorChange}
-//                 onBlur={handleEditorBlur}
-//                 onFocus={handleEditorFocus}
-//             />
-//         </div>
-//     );
-// };
+    const handleBoardChange = (event) => {
+        setBoard(event.target.value);
+    };
 
-// export default ArticleEditor;
+    const handleLocationChange = (event) => {
+        setLocation(event.target.value);
+    };
+
+    const handleTopicChange = (event) => {
+        setTopic(event.target.value);
+    };
+
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleContentChange = (event) => {
+        const inputValue = event.target.value;
+        setContent(inputValue);
+    
+        // 當超過1000個字符時，將字數標記為紅色
+        if (inputValue.length > 1000) {
+            alert('已超過1000字限制！');
+            setExceedLimit(true);
+        } else {
+            setExceedLimit(false);
+        }
+    };
+
+    const handleSubmit = async (event) => {
+        const ArticleData = new FormData();
+        ArticleData.append('Discussion_board_area', Board)
+        ArticleData.append('board_text_id', Location)
+        ArticleData.append('Text_type', Topic)
+        ArticleData.append('Text_title', Title)
+        ArticleData.append('Text', content)
+        imagesData.forEach((image,index) => {
+            ArticleData.append(`images[${index}]`,image);
+        });
+        event.preventDefault();
+        // test用
+        // console.log(ArticleData.get('Discussion_board_area'));
+        // console.log(ArticleData);
+        // // Here you can perform the submission logic
+        // console.log('Submitted:', {
+        //     Board,
+        //     Location,
+        //     Topic,
+        //     content,
+        // });
+    };
+
+    return (
+        <div>
+            <form className='row' onSubmit={handleSubmit}>
+                <h1>文章發布</h1>
+                <div className="col-md select-board">
+                    <label htmlFor="board" className="form-label">選擇討論版</label>
+                    <select id="board" className="form-select" value={Board} onChange={handleBoardChange}>
+                        <option disabled>請選擇</option>
+                        <option value="1">北部討論版</option>
+                        <option value="2">中部討論版</option>
+                        <option value="3">南部討論版</option>
+                        <option value="4">花東離島討論版</option>
+                    </select>
+                </div>
+                <div className="col-md select-location">
+                    <label htmlFor="location" className="form-label">選擇地點</label>
+                    <select id="location" className="form-select" value={Location} onChange={handleLocationChange}>
+                        <option disabled>請選擇</option>
+                        <option value="1">臺北市</option>
+                        <option value="2">新北市</option>
+                        <option value="3">宜蘭縣</option>
+                        <option value="4">基隆市</option>
+                        <option value="5">桃園市</option>
+                        <option value="6">新竹市</option>
+                        <option value="7">新竹縣</option>
+                        <option value="8">臺中市</option>
+                        <option value="9">苗栗縣</option>
+                        <option value="10">彰化縣</option>
+                        <option value="11">南投縣</option>
+                        <option value="12">雲林縣</option>
+                        <option value="13">嘉義市</option>
+                        <option value="14">嘉義縣</option>
+                        <option value="15">臺南市</option>
+                        <option value="16">高雄市</option>
+                        <option value="17">屏東縣</option>
+                        <option value="18">花蓮縣</option>
+                        <option value="19">臺東縣</option>
+                        <option value="20">澎湖縣</option>
+                        <option value="21">金門縣</option>
+                        <option value="22">連江縣</option>
+                    </select>
+                </div>
+                <div className="col-md select-topic">
+                    <label htmlFor="topic" className="form-label">選擇主題</label>
+                    <select id="topic" className="form-select" value={Topic} onChange={handleTopicChange}>
+                        <option disabled>請選擇</option>
+                        <option value="1">親子</option>
+                        <option value="2">露營</option>
+                        <option value="3">登山</option>
+                        <option value="4">老街</option>
+                        <option value="5">溫泉</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="Title" className="addtitle">
+                        <h3>文章名稱</h3>
+                        <input id="Title" className="addtitle_title" placeholder="請輸入文章名稱" onChange={handleTitleChange} />
+                    </label>
+                </div>
+                <div>
+                    <label htmlFor="content" className={`addtext_text ${exceedLimit ? 'exceed-limit' : ''}`}>
+                        <h3>文章內容</h3>
+                        <textarea id="content" className="addtext_text" placeholder="請輸入文字" onChange={handleContentChange} />
+                        <div className="addtext">
+                            字數：{content.length} / 1000
+                        </div>
+                    </label>
+                </div>
+                <label htmlFor="file-input">
+                    <p className='fs-4 mb-2'>選擇相片</p>
+                    <input
+                        type="file"
+                        id="file-input"
+                        accept="image/*"
+                        className="d-none ab"
+                        multiple
+                        onChange={handleUpload}
+                        ref={inputRef}
+                    />
+                    <span className='btn btn-outline-primary me-2'>上傳圖片</span>
+                    <span className='btn btn-outline-danger' onClick={(e) => handleRemoveAll(e)}>移除全部圖片</span>
+                </label>
+                <div className='row row-cols-2 row-cols-lg-3 mt-3 g-3'>
+                    {images.map((image, index) => {
+                        return (
+                            <div className='col' key={index}>
+                                <img className='selectedImg w-100' src={image.url} />
+                                <div className='imgInfo'>
+                                    <div className='fileName w-100 d-flex justify-content-between align-items-center'>
+                                        <p className='m-0 py-2 text-center'>{image.name}</p>
+                                        <button className='btn remove' onClick={(e) => handleRemove(e, index)}><i className="bi bi-x-circle fs-4"></i></button>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+
+                </div>
+                <div className='post_article'>
+                    <button className='btn btn-primary' type="submit" disabled={exceedLimit}>我要發文</button>
+                </div>
+            </form>
+
+        </div>
+    );
+};
+
+export default ArticleEditor;
--- frontend/src/pages/AddArticle.js
+++ frontend/src/pages/AddArticle.js
@@ -1,4 +1,5 @@
 import '../css/AddArticle.css';
+import ArticleEditor from '../components/ArticleEditor';
 
 
 export default function AddArticle() {
@@ -6,12 +7,8 @@ export default function AddArticle() {
     return (
         <div className="main">
             <div className="container">
-                <div className="top-post_article">
-                    <button className="btn btn-primary">我要發文</button>
-                </div>
                 <div className="article_editor">
-                    {/* <ArticleEditor></ArticleEditor> */}
-                    
+                    <ArticleEditor></ArticleEditor>
                 </div>
             </div>
         </div>

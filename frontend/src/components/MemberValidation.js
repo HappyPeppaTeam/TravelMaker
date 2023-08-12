49b02505b3194bdd611c15d112e594738f09578f
ga035756 <G12040720a>
2023-08-12 13:07:57 +0800

    add registerForm last check

--- frontend/src/components/MemberValidation.js
+++ frontend/src/components/MemberValidation.js
@@ -56,4 +56,48 @@ const memberValidateField = (fieldName, value,passowrd) => {
   return errorMessage;
 };
 
-export { memberValidateField }; // 匯出 memberValidateField 函式
+const registerFormValidate = (userData) => {
+  
+  let valid = true;
+
+      if (!userData.username) {
+        valid=false;
+      }
+      
+      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!userData.email) {
+        valid=false;
+      } else if (!emailPattern.test(userData.email)) {
+        valid=false;
+      }
+
+      if (!userData.password) {
+        valid=false;
+      } else if (userData.password.length < 6) {
+        valid=false;
+      }
+  
+      if (!userData.confirmPassword) {
+        valid=false;
+      } else if (userData.confirmPassword !== userData.password) {
+        valid=false;
+      }
+
+      if (!userData.fullName) {
+        valid=false;
+      }
+
+      if (!userData.nickName) {
+        valid=false;
+      }
+
+      if (!userData.birthday) {
+        valid=false;
+      }
+
+      if (!userData.gender) {
+        valid=false;
+      }
+  return valid;
+};
+export { memberValidateField ,registerFormValidate}; // 匯出 memberValidateField 函式
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { memberValidateField } from './MemberValidation'
+import { memberValidateField,registerFormValidate } from './MemberValidation'
 
 const RegisterModal = ({ onResponse, closeRegisterModal, openMessageToast }) => {
   const [username, setUsername] = useState('');
@@ -11,6 +11,7 @@ const RegisterModal = ({ onResponse, closeRegisterModal, openMessageToast }) => 
   const [email, setEmail] = useState('');
   const [birthday, setBirthday] = useState('');
   const [gender, setGender] = useState('');
+  const [registerError, setRegisterError] = useState('');
   const [errors, setErrors] = useState({
     username: '',
     email: '',
@@ -31,7 +32,7 @@ const RegisterModal = ({ onResponse, closeRegisterModal, openMessageToast }) => 
 
   const handleBlur = (event) => {
     const { id, value } = event.target;
-    const errorMessage = memberValidateField(id, value,password)
+    const errorMessage = memberValidateField(id, value, password)
     setErrors({
       ...errors,
       [id]: errorMessage,
@@ -69,46 +70,46 @@ const RegisterModal = ({ onResponse, closeRegisterModal, openMessageToast }) => 
     }
     return <span style={{ color }}>{label}</span>;
   };
-
-
-
   const register = async () => {
-    const userData = {
-      username,
-      password,
-      confirmPassword,
-      fullName,
-      nickName,
-      email,
-      birthday,
-      gender,
-    };
+      const userData = {
+        username,
+        password,
+        confirmPassword,
+        fullName,
+        nickName,
+        email,
+        birthday,
+        gender,
+      };
+      if (registerFormValidate(userData)) {
+      try {
+        const response = await axios.post('http://localhost/TravelMaker/Backend/public/api/register', userData, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          withCredentials: true,
+        });
 
-    try {
-      const response = await axios.post('http://localhost/TravelMaker/Backend/public/api/register', userData, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        withCredentials: true,
-      });
-
-      if (!response.status === 200) {
-        throw new Error('Network response was not ok');
+        if (!response.status === 200) {
+          throw new Error('Network response was not ok');
+        }
+        // window.location.reload()
+        // var token = response.data.token;
+        // document.cookie = `token=${token}; expires=${new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toUTCString()}; path=/localhost:3000`;
+        // sessionStorage.setItem('username', userData.username);
+        const data = response.data.message;
+        // Handle the response from the backend, if needed
+        onResponse(data);
+        closeRegisterModal();
+        openMessageToast();
+        console.log('Response from backend:', data);
+      } catch (error) {
+        // Handle error, if any
+        closeRegisterModal();
+        console.error(error);
       }
-      // window.location.reload()
-      // var token = response.data.token;
-      // document.cookie = `token=${token}; expires=${new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toUTCString()}; path=/localhost:3000`;
-      // sessionStorage.setItem('username', userData.username);
-      const data = response.data.message;
-      // Handle the response from the backend, if needed
-      onResponse(data);
-      closeRegisterModal();
-      openMessageToast();
-      console.log('Response from backend:', data);
-    } catch (error) {
-      // Handle error, if any
-      closeRegisterModal();
-      console.error(error);
+    }else{
+      setRegisterError('請檢查資料是否輸入正確');
     }
   }
 
@@ -142,61 +143,61 @@ const RegisterModal = ({ onResponse, closeRegisterModal, openMessageToast }) => 
                 value={password}
                 onChange={handlePasswordChange}
                 onBlur={handleBlur} />
-              <div className="ms-auto">{getPasswordStrengthLabel(passwordStrength)}</div>
+              <div className="mx-3">密碼強度：{getPasswordStrengthLabel(passwordStrength)}</div>
             </div>
             <div className="error-message text-danger">{errors.password}</div>
             <div className="input-group mb-3">
               <span className="input-group-text" id="basic-addon1">確認密碼</span>
-              <input type="password" className="form-control" id="confirmPassword" 
-              placeholder="Confirm Password" aria-label="Confirmassword" aria-describedby="basic-addon1" 
-              value={confirmPassword} 
-              onChange={(e) => setConfirmPassword(e.target.value)} 
-              onBlur={handleBlur}/>
+              <input type="password" className="form-control" id="confirmPassword"
+                placeholder="Confirm Password" aria-label="Confirmassword" aria-describedby="basic-addon1"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                onBlur={handleBlur} />
             </div>
             <div className="error-message text-danger">{errors.confirmPassword}</div>
             <div className="input-group mb-3">
               <span className="input-group-text" id="basic-addon1">會員姓名</span>
-              <input type="text" className="form-control" id="fullName" 
-              placeholder="FullName" aria-label="fullName" aria-describedby="basic-addon1" 
-              value={fullName} 
-              onChange={(e) => setFullName(e.target.value)} 
-              onBlur={handleBlur}/>
+              <input type="text" className="form-control" id="fullName"
+                placeholder="FullName" aria-label="fullName" aria-describedby="basic-addon1"
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
+                onBlur={handleBlur} />
             </div>
             <div className="error-message text-danger">{errors.fullName}</div>
             <div className="input-group mb-3">
               <span className="input-group-text" id="basic-addon1">暱稱</span>
-              <input type="text" className="form-control" id="nickName" 
-              placeholder="NickName" aria-label="nickName" aria-describedby="basic-addon1" 
-              value={nickName} 
-              onChange={(e) => setNickName(e.target.value)} 
-              onBlur={handleBlur}/>
+              <input type="text" className="form-control" id="nickName"
+                placeholder="NickName" aria-label="nickName" aria-describedby="basic-addon1"
+                value={nickName}
+                onChange={(e) => setNickName(e.target.value)}
+                onBlur={handleBlur} />
             </div>
             <div className="error-message text-danger">{errors.nickName}</div>
             <div className="input-group mb-3">
               <span className="input-group-text" id="basic-addon1">Email</span>
-              <input type="email" className="form-control" id="email" 
-              placeholder="Email" aria-label="email" aria-describedby="basic-addon1" 
-              value={email} 
-              onChange={(e) => setEmail(e.target.value)}
-              onBlur={handleBlur} />
+              <input type="email" className="form-control" id="email"
+                placeholder="Email" aria-label="email" aria-describedby="basic-addon1"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                onBlur={handleBlur} />
             </div>
             <div className="error-message text-danger">{errors.email}</div>
             <div className="input-group mb-3">
               <span className="input-group-text" id="basic-addon1">生日</span>
-              <input type="date" className="form-control" id="birthday" 
-              placeholder="birthday" aria-label="birthday" aria-describedby="basic-addon1" 
-              value={birthday} 
-              onChange={(e) => setBirthday(e.target.value)} 
-              onBlur={handleBlur}/>
+              <input type="date" className="form-control" id="birthday"
+                placeholder="birthday" aria-label="birthday" aria-describedby="basic-addon1"
+                value={birthday}
+                onChange={(e) => setBirthday(e.target.value)}
+                onBlur={handleBlur} />
             </div>
             <div className="error-message text-danger">{errors.birthday}</div>
             <div className="input-group mb-3">
               <span className="input-group-text" id="basic-addon1">性別</span>
-              <select className="form-select" name="gender" id="gender" 
-              aria-label="Default select example" 
-              value={gender} 
-              onChange={(e) => setGender(e.target.value)}
-              onBlur={handleBlur}>
+              <select className="form-select" name="gender" id="gender"
+                aria-label="Default select example"
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+                onBlur={handleBlur}>
                 <option value="">選取性別</option>
                 <option value="1">男</option>
                 <option value="2">女</option>
@@ -210,8 +211,10 @@ const RegisterModal = ({ onResponse, closeRegisterModal, openMessageToast }) => 
               快速註冊：<i className="bi bi-google" onClick={handleGoogle}></i>
             </div>
             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">取消</button>
-            <button type="button" className="btn btn-primary" onClick={register} data-bs-dismiss="modal">確認</button>
+            <button type="button" className="btn btn-primary" onClick={register}>確認</button>
+            <div className="error-message text-danger">{registerError}</div>
           </div>
+          {/* data-bs-dismiss="modal" */}
         </div>
       </div>
     </div>
